1046708dd475d27001579166853bafedff7582f0
Zahid Aziz <azizz2@montclair.edu>
2017-02-21 00:03:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/zee/wk5_fragment/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.zee.wk5_fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/com/example/zee/wk5_fragment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.zee.wk5_fragment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mobeToTwo(View view)
+    {
+        startActivity(new Intent(this,Main2Activity.class));
+    }
+}
